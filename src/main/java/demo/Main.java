e9951c533c7a3c7d7ff85fae4ba61479ad5a000f
YunChen <1183728554@qq.com>
2022-06-23 23:47:27 +0800

    first commit

---
+++ src/main/java/demo/Main.java
@@ -0,0 +1,10 @@
+package demo;
+
+import demo.entities.Student;
+
+public class Main {
+    public static void main(String[] args) {
+        Student xiaoming = new Student("1", "xiaoming", 18, "男");
+        System.out.println(xiaoming);
+    }
+}
